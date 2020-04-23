59c4bc70b7688cd1934897fa98440cc4a4090daa
realavalanche <sid.317@gmail.com>
2020-04-23 15:34:47 +0530

    added comments on thunk

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,9 +1,9 @@
-export const fetchData = (studentDetails) => {
-    return dispatch => {
+export const fetchData = (studentDetails) => { // action is returning a function (thunk) instead of a plain JS object
+    return dispatch => { // thunk takes in parameters dispatch function & getState function; returns a promise
         // setTimeout(() => {
         //     dispatch({ type: 'ADD_STUDENT', studentDetails })
         // }, 3000);
-        return fetch('http://jsonplaceholder.typicode.com/users').then(response => response.json()).then((data) => {
+        return fetch('http://jsonplaceholder.typicode.com/users').then(response => response.json()).then((data) => { //.then always returns a promise
             // console.log(data)
             dispatch({ type: 'ADD_STUDENT', studentDetails })
         })
